deb6bcedde579c9846f808906fdebf170d8909cd
Bruno Felix De Sousa <brunofelixdesousa@hotmail.com>
2026-03-12 22:47:38 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+export function App() {
+
+  return (
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="text-4xl font-bold text-gray-800">Hello, World!</h1>
+      </div>
+  )
+}
